b844da1d34252d9a2b58b1138a334bb78e0bc569
Brendan <brendanmarko@gmail.com>
2016-12-03 12:53:35 -0500

    Update Reflector.hpp

--- Code/Headers/Reflector.hpp
+++ Code/Headers/Reflector.hpp
@@ -8,23 +8,21 @@
 
 class Reflector 
 {
-
 	public:
 	Reflector();
 	Reflector(std::string);
 	
 	public:
-	void 					viewReflector();
-	void 					getFileContents();
-	void 					buildAlphabet(std::string);
-	void 					setReflectorLetter(char&);
-	int						findLetterPos(char&);
+	void 			viewReflector();
+	void 			getFileContents();
+	void 			buildAlphabet(std::string);
+	void 			setReflectorLetter(char&);
+	int			findLetterPos(char&);
 
 	private:
 	std::array<char, 26>	reflector_board;
-	std::string				file_path;
-	std::string 			reflector_contents;
-
+	std::string		file_path;
+	std::string 		reflector_contents;
 };
 
 #endif 
