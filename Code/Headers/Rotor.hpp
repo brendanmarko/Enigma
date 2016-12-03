84e7c7b7f2c336272072a8287359a9db7e15e75f
Brendan <brendanmarko@gmail.com>
2016-12-03 12:54:36 -0500

    Update Rotor.hpp

--- Code/Headers/Rotor.hpp
+++ Code/Headers/Rotor.hpp
@@ -13,30 +13,30 @@ class Rotor
 	Rotor(std::string);
 
 	public:
-	int 					getCurrPos();
-	void 					rotateRotor();
-	void 					buildAlphabet(std::string);
-	void 					printAlphabet();
-	void 					getFileContents();
-	int 					getRotations();
-	void 					setStartPos(char);
-	void 					lowerConvert(char&);
-	int 					findLetterPos(char);
-	char 					inverseLetter(int);
-	char 					retrieveLetter(int);
-	void 					buildInverse();
-	char 					convertPosToChar(int);
-	void 					printInverse();
-	void 					shiftAlphabet();
-	void 					resetCheck(int&, int&);
+	int 			getCurrPos();
+	void 			rotateRotor();
+	void 			buildAlphabet(std::string);
+	void 			printAlphabet();
+	void 			getFileContents();
+	int 			getRotations();
+	void 			setStartPos(char);
+	void 			lowerConvert(char&);
+	int 			findLetterPos(char);
+	char 			inverseLetter(int);
+	char 			retrieveLetter(int);
+	void			buildInverse();
+	char 			convertPosToChar(int);
+	void 			printInverse();
+	void 			shiftAlphabet();
+	void 			resetCheck(int&, int&);
 
 	private:
-	int						a;
-	int 					curr_pos;
-	int 					rotations;
-	std::string 			file_path;
-	std::string				rotor_value;
-	std::string 			rotor_contents;
+	int			a;
+	int 			curr_pos;
+	int 			rotations;
+	std::string 		file_path;
+	std::string		rotor_value;
+	std::string 		rotor_contents;
 	std::array<char, 26>	alphabet;
 	std::array<char, 26>	inverse_alphabet;
 };
