d207e4587d0ddf444e720acc07c747a461d385fc
Brendan <brendanmarko@gmail.com>
2016-12-03 12:52:28 -0500

    Update KeySheet.hpp

--- Code/Headers/KeySheet.hpp
+++ Code/Headers/KeySheet.hpp
@@ -14,20 +14,20 @@ class KeySheet
 	KeySheet(std::string);
 
 	public:
-	void 									buildRotor(std::string);
-	void 									buildAlpha(std::string);
-	void 									buildPlugboard(std::string);
-	void 									getFileContents();
-	void 									viewInfo();
-	std::vector<int> 						getRotors();
-	std::vector<char> 						getAlphas();	
+	void 						buildRotor(std::string);
+	void 						buildAlpha(std::string);
+	void 						buildPlugboard(std::string);
+	void 						getFileContents();
+	void 						viewInfo();
+	std::vector<int> 				getRotors();
+	std::vector<char> 				getAlphas();	
 	std::vector<std::pair<char, char>>		getPlugboard();
 
 	private:
-	std::vector<int>						rotor_order;
-	std::vector<char>						start_chars;
+	std::vector<int>				rotor_order;
+	std::vector<char>				start_chars;
 	std::vector<std::pair<char, char>>		plugboard_swaps;
-	std::string 							file_path;
+	std::string 					file_path;
 };
 
 #endif
