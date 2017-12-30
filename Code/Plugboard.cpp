1d23d2fb4c6e0dfd7c2b18448adfe98ebe620d92
Brendan <brendanmarko@gmail.com>
2017-12-30 12:01:40 -0500

    Tabs to spaces

--- Code/Plugboard.cpp
+++ Code/Plugboard.cpp
@@ -2,24 +2,24 @@
 
 Plugboard::Plugboard()
 {
-	// empty
+  // empty
 }
 
 Plugboard::Plugboard(std::vector<std::pair<char, char>> board)
 {
-	letter_swaps=board;
+  letter_swaps=board;
 }
 
 // checkPlugboard(char&)
 // This function checks to see if a character has to be swapped passing through the Plugboard
 void Plugboard::checkPlugboard(char& c)
 {
-	for (int i=0; i<letter_swaps.size(); i++)
-	{
-		if (c == letter_swaps.at(i).first)
-			c = letter_swaps.at(i).second;
+  for (int i=0; i<letter_swaps.size(); i++)
+  {
+    if (c == letter_swaps.at(i).first)
+      c = letter_swaps.at(i).second;
 
-		else if (c == letter_swaps.at(i).second)
-			c = letter_swaps.at(i).first;
-	}
+    else if (c == letter_swaps.at(i).second)
+      c = letter_swaps.at(i).first;
+  }
 }
